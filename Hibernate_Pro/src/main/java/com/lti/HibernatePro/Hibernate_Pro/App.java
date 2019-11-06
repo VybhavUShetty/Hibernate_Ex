90d47778eda4bc094c19f7dfb8798e60b5e71e6a
nalini singh <vybhav65@gmail.com>
2019-11-06 01:52:16 -0800

    I have committed

---
+++ Hibernate_Pro/src/main/java/com/lti/HibernatePro/Hibernate_Pro/App.java
@@ -0,0 +1,13 @@
+package com.lti.HibernatePro.Hibernate_Pro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
