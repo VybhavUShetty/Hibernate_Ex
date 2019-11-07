827708efd3093235b1012b576c8c86474be6db88
Vybhav <vybhav65@gmail.com>
2019-11-07 08:41:21 +0530

    commit

---
+++ Student_Maven/src/main/java/com/lti/StudentMaven/App.java
@@ -0,0 +1,24 @@
+package com.lti.StudentMaven;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Configuration configuration=new Configuration().configure();
+       StandardServiceRegistryBuilder builder=new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+       SessionFactory fac=configuration.buildSessionFactory(builder.build());
+       Session session=fac.openSession();
+       Student student=new Student();
+     
+       student.setName("Vybhav");
+       student.setDept("Mech");
+       
+       session.beginTransaction();
+       session.save(student);
+    }
+}
---
+++ Student_Maven/src/main/java/com/lti/StudentMaven/Student.java
@@ -0,0 +1,28 @@
+package com.lti.StudentMaven;
+
+public class Student {
+	private int id;
+	private String name;
+	private String Dept;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDept() {
+		return Dept;
+	}
+	public void setDept(String dept) {
+		Dept = dept;
+	}
+	
+	
+
+}
